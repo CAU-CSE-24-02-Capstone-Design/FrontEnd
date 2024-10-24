b7b6b1734f4272038e3c188966993b32c9252e55
Hyun0828 <spdlqj4818@naver.com>
2024-10-24 21:02:59 +0900

    fix: api_url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const SPRING_API_URL = "https://peachmentor.com/api/spring";
-export const FASTAPI_API_URL = "https://peachmentor.com/api/fastapi";
+export const SPRING_API_URL = "http://peachmentor.com/api/spring";
+export const FASTAPI_API_URL = "http://peachmentor.com/api/fastapi";
