99224f208910e912ba52a3532d12122e31a2e909
Hyun0828 <spdlqj4818@naver.com>
2024-11-01 02:40:59 +0900

    [chore] 피드백 페이지로 넘어가는 버튼 생성

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -103,12 +103,18 @@ const Main = () => {
                         <p className="mb-10 text-xl font-semibold text-center text-grayscale-100">
                             답변 내용을 분석 중입니다.
                         </p>
-                        <ClockLoader color="#4A90E2" loading={loading} size={60}/>
+                        <ClockLoader color="#4A90E2" loading={loading} size={15}/>
                         <button
                             onClick={handleShowAISpeechPopup}
                             className="px-8 py-3 mt-10 text-lg font-semibold text-white rounded-full bg-primary-50"
                         >
                             AI 답변 보기
+                        </button>
+                        <button
+                            onClick={() => navigate("/recordscript")} // 화살표 함수로 변경
+                            className="px-8 py-3 mt-10 text-lg font-semibold text-white rounded-full bg-primary-50"
+                        >
+                            피드백 받기
                         </button>
                     </div>
                 )}
