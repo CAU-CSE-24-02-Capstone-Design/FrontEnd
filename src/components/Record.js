a7412c8de34ae359fe1d6b09386d2b3ea54d41e6
Hyun0828 <spdlqj4818@naver.com>
2024-11-01 01:05:19 +0900

    [chore] 빌드를 위한 오타 수정

--- src/components/Record.js
+++ src/components/Record.js
@@ -7,9 +7,73 @@ const Record = ({isRecording, answerId, questionText, onResponse}) => {
     const [stream, setStream] = useState(null); // 마이크에서 가져온 오디오 스트림을 저장
     const [media, setMedia] = useState(null); // MediaRecorder 객체를 저장하여 녹음을 관리
     const [onRec, setOnRec] = useState(true); // 녹음 중인지 여부를 추적
-    const [audioUrl, setAudioUrl] = useState(null); // 녹음된 오디오 데이터를 Blob으로 저장
     const audioContextRef = useRef(null); // AudioContext 참조
     const sourceRef = useRef(null); // MediaStreamSource 참조
+
+    // 오디오 파일 fastapi 서버로 전달하기
+    const sendAudioFile = useCallback(
+        async (sound) => {
+            try {
+                const formData = new FormData();
+                formData.append("file", sound);
+                formData.append("answerId", answerId);
+                formData.append("question", questionText);
+                const response = await instance.post(
+                    `${FASTAPI_API_URL}/record/insight`,
+                    formData,
+                    {
+                        headers: {"Content-Type": "multipart/form-data"},
+                    }
+                );
+
+                if (response.data.isSuccess) {
+                    onResponse(response.data.result.insight);
+                } else {
+                    console.error("인사이트 받아오기 오류:", response.data.message);
+                }
+            } catch (error) {
+                console.error("인사이트 받아오기 실패");
+            }
+        },
+        [answerId, questionText, onResponse]
+    );
+
+    // 오디오 파일 생성하기
+    const onSubmitAudioFile = useCallback(
+        async (wavBlob) => {
+            const sound = new File([wavBlob], "soundBlob.wav", {
+                lastModified: new Date().getTime(),
+                type: "audio/wave",
+            });
+            await sendAudioFile(sound);
+        },
+        [sendAudioFile]
+    );
+
+    // 녹음 중지
+    const stopRecording = useCallback(
+        async (mediaRecorder, source) => {
+            mediaRecorder.ondataavailable = async (e) => {
+                if (e.data && e.data.size > 0) {
+                    const wavBlob = await getWaveBlob(e.data, true);
+                    setAudioUrl(wavBlob);
+                    setOnRec(true);
+                    await onSubmitAudioFile(wavBlob);
+                }
+            };
+
+            stream.getAudioTracks().forEach((track) => track.stop());
+            mediaRecorder.stop();
+            source.disconnect();
+
+            if (audioContextRef.current) {
+                audioContextRef.current.close().then(() => {
+                    audioContextRef.current = null;
+                });
+            }
+        },
+        [stream]
+    );
 
     // 녹음 시작
     const onRecAudio = useCallback(async () => {
@@ -18,7 +82,7 @@ const Record = ({isRecording, answerId, questionText, onResponse}) => {
         }
 
         try {
-            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            const stream = await navigator.mediaDevices.getUserMedia({audio: true});
             const mediaRecorder = new MediaRecorder(stream);
             mediaRecorder.start();
             setStream(stream);
@@ -53,71 +117,6 @@ const Record = ({isRecording, answerId, questionText, onResponse}) => {
             stopRecording(media, sourceRef.current);
         }
     }, [media, stopRecording]);
-
-    // 녹음 중지
-    const stopRecording = useCallback(
-        async (mediaRecorder, source) => {
-            mediaRecorder.ondataavailable = async (e) => {
-                if (e.data && e.data.size > 0) {
-                    const wavBlob = await getWaveBlob(e.data, true);
-                    setAudioUrl(wavBlob);
-                    setOnRec(true);
-                    await onSubmitAudioFile(wavBlob);
-                }
-            };
-
-            stream.getAudioTracks().forEach((track) => track.stop());
-            mediaRecorder.stop();
-            source.disconnect();
-
-            if (audioContextRef.current) {
-                audioContextRef.current.close().then(() => {
-                    audioContextRef.current = null;
-                });
-            }
-        },
-        [stream]
-    );
-
-    // 오디오 파일 생성하기
-    const onSubmitAudioFile = useCallback(
-        async (wavBlob) => {
-            const sound = new File([wavBlob], "soundBlob.wav", {
-                lastModified: new Date().getTime(),
-                type: "audio/wave",
-            });
-            await sendAudioFile(sound);
-        },
-        [sendAudioFile]
-    );
-
-    // 오디오 파일 fastapi 서버로 전달하기
-    const sendAudioFile = useCallback(
-        async (sound) => {
-            try {
-                const formData = new FormData();
-                formData.append("file", sound);
-                formData.append("answerId", answerId);
-                formData.append("question", questionText);
-                const response = await instance.post(
-                    `${FASTAPI_API_URL}/record/insight`,
-                    formData,
-                    {
-                        headers: { "Content-Type": "multipart/form-data" },
-                    }
-                );
-
-                if (response.data.isSuccess) {
-                    onResponse(response.data.result.insight);
-                } else {
-                    console.error("인사이트 받아오기 오류:", response.data.message);
-                }
-            } catch (error) {
-                console.error("인사이트 받아오기 실패");
-            }
-        },
-        [answerId, questionText, onResponse]
-    );
 
     useEffect(() => {
         if (isRecording)
