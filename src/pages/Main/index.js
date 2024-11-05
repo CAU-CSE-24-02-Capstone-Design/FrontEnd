92515d395b7a7b22a0c7ca82e8516bc0f41b3ffb
Hyun0828 <spdlqj4818@naver.com>
2024-11-06 01:13:31 +0900

    [chore] no-unused-vars 제거

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
 import Countdown from "./components/Countdown";
