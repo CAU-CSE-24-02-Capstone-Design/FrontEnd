ed53fa9ff2f7ebed323e4c8ea31354c06df8c537
Hyun0828 <spdlqj4818@naver.com>
2024-11-20 15:10:17 +0900

    [fix] NavBar Button tag 분리

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,49 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {useNavigate} from "react-router-dom";
 import instance from "../axios/TokenInterceptor";
 import {SPRING_API_URL} from "../constants/api";
 
 const Header = () => {
     const navigate = useNavigate();
-    const [selfFeedback, setSelfFeedback] = useState(null);
-    const [isCompleteSpeech, setIsCompleteSpeech] = useState(false);
-
-    useEffect(() => {
-        const getDoAnswerToday = async () => {
-            try {
-                const response = await instance.get(`${SPRING_API_URL}/answers/completions`)
-                if (response.data.isSuccess) {
-                    if (response.data.code === "ANSWER4001" || response.data.code === "USER4002" || response.data.code === "ACCESSTOKEN4002") {
-                        console.error("오늘 답변 했는 지 여부 받아오기 API 서버 에러");
-                    } else {
-                        if (response.data.result.answerExists) {
-                            setIsCompleteSpeech(true);
-                        } else {
-                            setIsCompleteSpeech(false);
-                        }
-                        console.log("오늘 답변 했는 지 여부 받아오기 성공");
-                    }
-                } else {
-                    console.error("오늘 답변 했는 지 여부 받아오기 실패");
-                }
-            } catch (error) {
-                console.error("오늘 답변 했는 지 여부 받아오기 실패");
-            }
-        }
-
-        const getBeforeSelfFeedback = async () => {
-            try {
-                const response = await instance.get(`${SPRING_API_URL}/self-feedbacks/latest-feedbacks`);
-                if (response.data.isSuccess) {
-                    if (response.data.code === "ANSWER4001" || response.data.code === "SELFFEEDBACK4001") {
-                    } else {
-                        setSelfFeedback(response.data.result.feedback);
-                        console.log("이전 셀프 피드백 받아오기 성공");
-                    }
-                } else {
-                    console.error("이전 셀프 피드백 받아오기 실패");
-                }
-            } catch (error) {
-                console.error("이전 셀프 피드백 받아오기 실패");
-            }
-        };
-
-        getDoAnswerToday();
-        getBeforeSelfFeedback();
-    }, []);
 
     // Header의 복숭아멘토 텍스트 클릭 시 메인 페이지로 이동
-    const handleLogoClick = () => {
+    const handleLogoClick = async () => {
+        let isCompleteSpeech = false;
+        let selfFeedback = null;
+
+        try {
+            const response = await instance.get(`${SPRING_API_URL}/answers/completions`)
+            if (response.data.isSuccess) {
+                if (response.data.code === "ANSWER4001" || response.data.code === "USER4002" || response.data.code === "ACCESSTOKEN4002") {
+                    console.error("오늘 답변 했는 지 여부 받아오기 API 서버 에러");
+                } else {
+                    if (response.data.result.answerExists) {
+                        isCompleteSpeech = true;
+                    } else {
+                        isCompleteSpeech = false;
+                    }
+                    console.log("오늘 답변 했는 지 여부 받아오기 성공");
+                }
+            } else {
+                console.error("오늘 답변 했는 지 여부 받아오기 실패");
+            }
+        } catch (error) {
+            console.error("오늘 답변 했는 지 여부 받아오기 실패");
+        }
+        try {
+            const response = await instance.get(`${SPRING_API_URL}/self-feedbacks/latest-feedbacks`);
+            if (response.data.isSuccess) {
+                if (response.data.code === "ANSWER4001" || response.data.code === "SELFFEEDBACK4001") {
+                } else {
+                    selfFeedback = response.data.result.feedback;
+                    console.log("이전 셀프 피드백 받아오기 성공");
+                }
+            } else {
+                console.error("이전 셀프 피드백 받아오기 실패");
+            }
+        } catch (error) {
+            console.error("이전 셀프 피드백 받아오기 실패");
+        }
         navigate(`/main?selfFeedback=${selfFeedback}&isCompleteSpeech=${isCompleteSpeech}`);
     };
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,89 +1,90 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {useNavigate} from "react-router-dom";
 import instance from "../axios/TokenInterceptor";
 import {SPRING_API_URL} from "../constants/api";
 
 const NavBar = () => {
     const navigate = useNavigate();
-    const [selfFeedback, setSelfFeedback] = useState(null);
-    const [isCompleteSpeech, setIsCompleteSpeech] = useState(false);
 
-    useEffect(() => {
-        const getDoAnswerToday = async () => {
-            try {
-                const response = await instance.get(`${SPRING_API_URL}/answers/completions`)
-                if (response.data.isSuccess) {
-                    if (response.data.code === "ANSWER4001" || response.data.code === "USER4002" || response.data.code === "ACCESSTOKEN4002") {
-                        console.error("오늘 답변 했는 지 여부 받아오기 API 서버 에러");
-                    } else {
-                        if (response.data.result.answerExists) {
-                            setIsCompleteSpeech(true);
-                        } else {
-                            setIsCompleteSpeech(false);
-                        }
-                        console.log("오늘 답변 했는 지 여부 받아오기 성공");
-                    }
+    const handleMainButton = async () => {
+        let isCompleteSpeech = false;
+        let selfFeedback = null;
+
+        try {
+            const response = await instance.get(`${SPRING_API_URL}/answers/completions`)
+            if (response.data.isSuccess) {
+                if (response.data.code === "ANSWER4001" || response.data.code === "USER4002" || response.data.code === "ACCESSTOKEN4002") {
+                    console.error("오늘 답변 했는 지 여부 받아오기 API 서버 에러");
                 } else {
-                    console.error("오늘 답변 했는 지 여부 받아오기 실패");
+                    if (response.data.result.answerExists) {
+                        isCompleteSpeech = true;
+                    } else {
+                        isCompleteSpeech = false;
+                    }
+                    console.log("오늘 답변 했는 지 여부 받아오기 성공");
                 }
-            } catch (error) {
+            } else {
                 console.error("오늘 답변 했는 지 여부 받아오기 실패");
             }
+        } catch (error) {
+            console.error("오늘 답변 했는 지 여부 받아오기 실패");
         }
-
-        const getBeforeSelfFeedback = async () => {
-            try {
-                const response = await instance.get(`${SPRING_API_URL}/self-feedbacks/latest-feedbacks`);
-                if (response.data.isSuccess) {
-                    if (response.data.code === "ANSWER4001" || response.data.code === "SELFFEEDBACK4001") {
-                    } else {
-                        setSelfFeedback(response.data.result.feedback);
-                        console.log("이전 셀프 피드백 받아오기 성공");
-                    }
+        try {
+            const response = await instance.get(`${SPRING_API_URL}/self-feedbacks/latest-feedbacks`);
+            if (response.data.isSuccess) {
+                if (response.data.code === "ANSWER4001" || response.data.code === "SELFFEEDBACK4001") {
                 } else {
-                    console.error("이전 셀프 피드백 받아오기 실패");
+                    selfFeedback = response.data.result.feedback;
+                    console.log("이전 셀프 피드백 받아오기 성공");
                 }
-            } catch (error) {
+            } else {
                 console.error("이전 셀프 피드백 받아오기 실패");
             }
-        };
-
-        getDoAnswerToday();
-        getBeforeSelfFeedback();
-    }, []);
-
-    // 하단 바 버튼 경로, 이미지, 텍스트를 저장하는 배열
-    const menuItems = [
-        {path: "/calendar", icon: "/webp/calendar_button.webp", label: "캘린더"},
-        {
-            path: `/main?selfFeedback=${selfFeedback}&isCompleteSpeech=${isCompleteSpeech}`,
-            icon: "/webp/home_button.webp",
-            label: "홈"
-        },
-        {
-            path: "/statistics",
-            icon: "/webp/statistics_button.webp",
-            label: "통계",
-        },
-    ];
+        } catch (error) {
+            console.error("이전 셀프 피드백 받아오기 실패");
+        }
+        navigate(`/main?selfFeedback=${selfFeedback}&isCompleteSpeech=${isCompleteSpeech}`);
+    }
 
     return (
         <nav
-            className="bottom-0 left-0 w-full max-w-[500px] mx-auto flex justify-around border-t border-slate-500 py-3">
-            {menuItems.map((item, index) => (
-                <button
-                    key={index}
-                    onClick={() => navigate(item.path)}
-                    className="flex flex-col items-center"
-                >
-                    <img
-                        src={item.icon}
-                        alt={`${item.label} Icon`}
-                        className="w-6 h-6 mb-1"
-                    />
-                    <span className="text-base font-paperlogy-title">{item.label}</span>
-                </button>
-            ))}
+            className="bottom-0 left-0 w-full max-w-[500px] mx-auto flex justify-around border-t border-slate-500 py-3"
+        >
+            <button
+                onClick={() => navigate("/calendar")}
+                className="flex flex-col items-center"
+            >
+                <img
+                    src="/webp/calendar_button.webp"
+                    alt="캘린더 Icon"
+                    className="w-6 h-6 mb-1"
+                />
+                <span className="text-base font-paperlogy-title">캘린더</span>
+            </button>
+
+            <button
+                onClick={handleMainButton}
+                className="flex flex-col items-center"
+            >
+                <img
+                    src="/webp/home_button.webp"
+                    alt="홈 Icon"
+                    className="w-6 h-6 mb-1"
+                />
+                <span className="text-base font-paperlogy-title">홈</span>
+            </button>
+
+            <button
+                onClick={() => navigate("/statistics")}
+                className="flex flex-col items-center"
+            >
+                <img
+                    src="/webp/statistics_button.webp"
+                    alt="통계 Icon"
+                    className="w-6 h-6 mb-1"
+                />
+                <span className="text-base font-paperlogy-title">통계</span>
+            </button>
         </nav>
     );
 };
