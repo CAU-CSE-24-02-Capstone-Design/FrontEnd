c329b7a5182c0f73a168aa8bdb91c8ab7343e3c5
Hyun0828 <spdlqj4818@naver.com>
2024-11-20 12:29:06 +0900

    [feat] 로그인 후 메인페이지로 넘어갈 때 상태가 아니라 지역변수를 통해 전달

--- src/components/oauth/KakaoRedirectPage.js
+++ src/components/oauth/KakaoRedirectPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect} from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 import axios from "axios";
 import {SPRING_API_URL} from "../../constants/api";
@@ -10,8 +10,45 @@ const KakaoRedirectPage = () => {
     const location = useLocation();
     const navigate = useNavigate();
 
-    const [selfFeedback, setSelfFeedback] = useState(null);
-    const [isCompleteSpeech, setIsCompleteSpeech] = useState(false);
+    const navigateMain = useCallback(async () => {
+        let isCompleteSpeech = false;
+        let selfFeedback = null;
+
+        try {
+            const response = await instance.get(`${SPRING_API_URL}/answers/completions`)
+            if (response.data.isSuccess) {
+                if (response.data.code === "ANSWER4001" || response.data.code === "USER4002" || response.data.code === "ACCESSTOKEN4002") {
+                    console.error("오늘 답변 했는 지 여부 받아오기 API 서버 에러");
+                } else {
+                    if (response.data.result.answerExists) {
+                        isCompleteSpeech = true;
+                    } else {
+                        isCompleteSpeech = false;
+                    }
+                    console.log("오늘 답변 했는 지 여부 받아오기 성공");
+                }
+            } else {
+                console.error("오늘 답변 했는 지 여부 받아오기 실패");
+            }
+        } catch (error) {
+            console.error("오늘 답변 했는 지 여부 받아오기 실패");
+        }
+        try {
+            const response = await instance.get(`${SPRING_API_URL}/self-feedbacks/latest-feedbacks`);
+            if (response.data.isSuccess) {
+                if (response.data.code === "ANSWER4001" || response.data.code === "SELFFEEDBACK4001") {
+                } else {
+                    selfFeedback = response.data.result.feedback;
+                    console.log("이전 셀프 피드백 받아오기 성공");
+                }
+            } else {
+                console.error("이전 셀프 피드백 받아오기 실패");
+            }
+        } catch (error) {
+            console.error("이전 셀프 피드백 받아오기 실패");
+        }
+        navigate(`/main?selfFeedback=${selfFeedback}&isCompleteSpeech=${isCompleteSpeech}`);
+    }, [navigate]);
 
     useEffect(() => {
         const handleOAuthKakao = async (code) => {
@@ -30,7 +67,7 @@ const KakaoRedirectPage = () => {
                     if (role === "GUEST")
                         navigate("/guestrecord");
                     else if (role === "USER") {
-                        await Promise.all([getDoAnswerToday(), getBeforeSelfFeedback()]);
+                        navigateMain();
                     }
                 } else {
                     console.error("OAuth2 로그인 오류");
@@ -47,51 +84,7 @@ const KakaoRedirectPage = () => {
         if (code) {
             handleOAuthKakao(code);
         }
-    }, [location, navigate]); // 의존성 배열에서 navigate 추가
-
-    const getDoAnswerToday = async () => {
-        try {
-            const response = await instance.get(`${SPRING_API_URL}/answers/completions`);
-            if (response.data.isSuccess) {
-                if (response.data.code === "ANSWER4001" || response.data.code === "USER4002" || response.data.code === "ACCESSTOKEN4002") {
-                    console.error("오늘 답변 했는 지 여부 받아오기 API 서버 에러");
-                } else {
-                    if (response.data.result.answerExists) {
-                        setIsCompleteSpeech(true);
-                    } else {
-                        setIsCompleteSpeech(false);
-                    }
-                    console.log("오늘 답변 했는 지 여부 받아오기 성공");
-                }
-            } else {
-                console.error("오늘 답변 했는 지 여부 받아오기 실패");
-            }
-        } catch (error) {
-            console.error("오늘 답변 했는 지 여부 받아오기 실패", error);
-        }
-    };
-
-    const getBeforeSelfFeedback = async () => {
-        try {
-            const response = await instance.get(`${SPRING_API_URL}/self-feedbacks/latest-feedbacks`);
-            if (response.data.isSuccess) {
-                if (response.data.code === "ANSWER4001" || response.data.code === "SELFFEEDBACK4001") {
-                    console.log("이전 셀프 피드백 없음");
-                } else {
-                    setSelfFeedback(response.data.result.feedback);
-                    console.log("이전 셀프 피드백 받아오기 성공");
-                }
-            } else {
-                console.error("이전 셀프 피드백 받아오기 실패");
-            }
-        } catch (error) {
-            console.error("이전 셀프 피드백 받아오기 실패", error);
-        }
-    };
-
-    useEffect(() => {
-        navigate(`/main?selfFeedback=${selfFeedback}&isCompleteSpeech=${isCompleteSpeech}`);
-    }, [selfFeedback, isCompleteSpeech, navigate]); // selfFeedback, isCompleteSpeech 값 변경될 때마다 호출
+    }, [location, navigate, navigateMain]); // 의존성 배열에서 navigate 추가
 
 
     return <div>Processing...</div>;
